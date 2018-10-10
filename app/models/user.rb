96dc15b885d3008092f626d0f1bc0fcb92b89170
Amanda Mitchell <amandamitchell707@gmail.com>
2018-10-09 21:11:13 -0700

    Add users sessions and root controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,50 @@
 class User < ApplicationRecord
-  validates :email, :session_token, :password_digest, unique: true
-  validates :password, length { minimum: 6, allow_nil: true }
+  validates :email, presence: true, uniqueness: true
+  validates :session_token, :password_digest, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after initialize :ensure_session_token
 
   attr_reader :password
 
+  def password=(pw)
+    @password = pw
+    self.password_digest = BCrypt::Password.create(pw)
+  end
+
+  def is_password?(pw)
+    BCrypt::Password.new(self.password_digest).is_password?(pw)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  # has_many :routes,
+  # foreign_key: :author_id,
+  # class_name: :Route
+
+  has_many :friends,
+    foreign_key: :friender_id,
+    class_name: :User
+
+  has_many :friends,
+    foreign_key: :friendee_id,
+    class_name: :User
 end
