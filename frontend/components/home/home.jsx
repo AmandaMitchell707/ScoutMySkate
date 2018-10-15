6603cf967059ee411c2646d8a9889f457860ccbc
Amanda Mitchell <amandamitchell707@gmail.com>
2018-10-15 11:06:38 -0700

    Hide google api key

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -11,6 +11,6 @@ export default () => (
         </h2>
       </div>
     </div>
-    <div>Icons made by <a href="assets/street-skater.png" title="Baianat">Baianat</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+    <div>Icons made by Baianat from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> and licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
   </div>
 );
--- frontend/components/new_route_map.jsx
+++ frontend/components/new_route_map.jsx
@@ -6,6 +6,7 @@ class NewRouteMap extends React.Component {
   }
 
   componentDidMount() {
+
     const mapOptions = {
       center: { lat: 37.7758, lng: -122.435 },
       zoom: 13,
@@ -13,17 +14,37 @@ class NewRouteMap extends React.Component {
 
     // wrap this.mapNode in a Google Map
     this.map = new google.maps.Map(this.mapNode, mapOptions);
+
+    // add App Academy marker
+    this.addMarker({ lat: 37.7990, lng: -122.4014 });
+
+    let mapCenter;
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        mapCenter = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
+
+        this.map.setCenter(mapCenter);
+      });
+    } else {
+      mapCenter = { lat: 37.7758, lng: -122.435 };
+    }
+    // google.maps.event.addListener(this.map, 'click',
+    //   (event => addMarker({ coords: event.latlng }));
+
   }
 
-  addMarker(coordinates) {
+  addMarker(coords) {
     let marker = new google.maps.Marker({
-      position: coordinates,
+      position: coords,
       map: this.map,
     });
   }
 
   render() {
-    this.addMarker({ lat: 37.7758, lng: -122.435 });
 
     return (
       <div id='map-container' ref={ map => this.mapNode = map }>
