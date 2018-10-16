8fa570eb4c80adb9a9adaa2b4e685e6312163d71
Amanda Mitchell <amandamitchell707@gmail.com>
2018-10-15 22:17:04 -0700

    Add waypoints to route and hide all marker flags except the first

--- frontend/components/routes/new_route_map.jsx
+++ frontend/components/routes/new_route_map.jsx
@@ -9,19 +9,16 @@ class NewRouteMap extends React.Component {
 
   componentDidMount() {
     this.initMap();
-
     // this.addMarker({ lat: 37.7990, lng: -122.4014 });
-    this.map.addListener('click', event => {
-      this.addMarker({ lat: event.latLng.lat(), lng: event.latLng.lng() });
-      this.calcRoute();
-    });
-
   }
 
   initMap() {
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+
     const mapOptions = {
       center: { lat: 37.7758, lng: -122.435 },
-      zoom: 13,
+      zoom: 15,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
     };
 
@@ -43,36 +40,61 @@ class NewRouteMap extends React.Component {
       });
     }
 
+    directionsDisplay.setMap(map);
     this.map = map;
+
+    this.map.addListener('click', event => {
+      this.addMarker({ lat: event.latLng.lat(), lng: event.latLng.lng() });
+      this.calcAndDisplayRoute(directionsService, directionsDisplay);
+    });
   }
 
   addMarker(coords) {
-    this.markers.push(coords);
+    let iconScale = 4;
+    if (this.markers.length > 0) {
+      iconScale = 0;
+    }
 
     let marker = new google.maps.Marker({
       position: coords,
       map: this.map,
+      icon: {
+            path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW,
+            scale: iconScale
+        }
     });
+
+    this.markers.push(marker);
   }
 
-  calcRoute() {
+  calcAndDisplayRoute(directionsService, directionsDisplay) {
     if (this.markers.length === 1) {
       return;
     }
 
-    let directionsService = new google.maps.DirectionsService();
-    let directionsDisplay = new google.maps.DirectionsRenderer();
-    directionsDisplay.setMap(this.map);
+    let start = this.markers[0].position;
+    // this.markers[0].icon = {
+    //   path: google.maps.SymbolPath.CIRCLE,
+    //   scale: 0
+    // }
+    let end = this.markers[this.markers.length - 1].position;
+    let waypoints = [];
 
-    let start = this.markers[this.markers.length - 2];
-    let end = this.markers[this.markers.length - 1];
+    for (var i = 1; i < this.markers.length - 1; i++) {
+      waypoints.push({
+        location: this.markers[i].position,
+        stopover: false
+      });
+    }
+
 
     const request = {
       origin: start,
+      waypoints: waypoints,
       destination: end,
       travelMode: google.maps.DirectionsTravelMode.WALKING,
     };
-    
+
     directionsService.route(request, (response, status) => {
       if (status == 'OK') {
         directionsDisplay.setDirections(response);
