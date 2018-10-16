a96458e098269b3ca04438026615662fad8f0e42
Amanda Mitchell <amandamitchell707@gmail.com>
2018-10-16 11:26:54 -0700

    Add skate route api utils

--- frontend/util/skate_route_api_util.js
+++ frontend/util/skate_route_api_util.js
@@ -1,7 +1,42 @@
-export const fetchSkateRoutes = () => (
+export const createSkateRoute = skateRoute => (
+  $.ajax({
+    url: '/api/skate_routes',
+    method: 'POST',
+    data: { skateRoute },
+    error: (err) => console.log(err),
+  })
+);
+
+export const fetchAllSkateRoutes = () => (
   $.ajax({
     url: '/api/skate_routes',
     method: 'GET',
+    error: (err) => console.log(err),
+  })
+);
+
+export const fetchSkateRoute = routeId => (
+  $.ajax({
+    url: '/api/skate_routes/${routeId}',
+    method: 'GET',
+    error: (err) => console.log(err),
+  })
+);
+
+
+export const updateSkateRoute = skateRoute => (
+  $.ajax({
+    url: '/api/skate_routes/${skateRoute.id}',
+    method: 'PATCH',
+    data: { skateRoute },
+    error: (err) => console.log(err),
+  })
+);
+
+export const deleteSkateRoute = routeId => (
+  $.ajax({
+    url: '/api/skate_routes/${routeId}',
+    method: 'DELETE',
     error: (err) => console.log(err),
   })
 );
