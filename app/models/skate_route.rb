7c007984ee06cc7a6584f767155620d2587ee1da
Amanda Mitchell <amandamitchell707@gmail.com>
2018-10-19 10:39:53 -0700

    Edit skate route model

--- app/models/skate_route.rb
+++ app/models/skate_route.rb
@@ -1,5 +1,5 @@
 class SkateRoute < ApplicationRecord
-  validates :author_id, :polyline, :distance, :name, :city, presence: true
+  validates :polyline, :distance, :name, :city, presence: true
 
   belongs_to :author,
     foreign_key: :author_id,
