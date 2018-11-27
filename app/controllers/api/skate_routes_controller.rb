93591c8734a0eadfcdd80dfe19c258dfec332672
Amanda Mitchell <amandamitchell707@gmail.com>
2018-11-27 14:56:18 -0800

    Add encoded markers to skate route params

--- app/controllers/api/skate_routes_controller.rb
+++ app/controllers/api/skate_routes_controller.rb
@@ -21,7 +21,7 @@ class Api::SkateRoutesController < ApplicationController
 
   def skate_route_params
     params.require(:skate_route).permit(
-      :author_id, :polyline, :distance, :name, :city
+      :author_id, :polyline, :distance, :name, :city, :encoded_markers
     ).transform_keys!(&:underscore)
   end
 end
