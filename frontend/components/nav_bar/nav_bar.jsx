fe34d0533c4a98f5a14037f7e527e61397007b58
Amanda Mitchell <amandamitchell707@gmail.com>
2018-12-12 12:27:05 -0800

    Add create route link to nav bar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 export default ({ currentUser, logout }) => {
   const display = currentUser ? (
     <div className="right-nav">
+      <Link className="nav-login-logout" to="/routes/create">Create Route</Link>
       <button className="nav-signup" onClick={logout}>Logout</button>
     </div>
   ) : (
@@ -18,6 +19,7 @@ export default ({ currentUser, logout }) => {
       <nav className="nav-bar-inner">
       <div className="left-nav">
         <Link className="logo" to="/">
+
           <img className="skate-logo" src={window.images.logo} alt="logo"></img>
           <h1 className="scout">SCOUT MY SKATE</h1>
         </Link>
