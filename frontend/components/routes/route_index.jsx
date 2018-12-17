2f8e1fa9da7727bb78272a2392aa2928b92e6580
Amanda Mitchell <amandamitchell707@gmail.com>
2018-12-17 12:32:04 -0800

    Finish route index container

--- frontend/components/routes/route_index.jsx
+++ frontend/components/routes/route_index.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 
+import RouteIndexItem from './route_index_item';
+
 class RouteIndex extends React.Component {
   componentDidMount() {
     this.props.fetchAllSkateRoutes();
   }
 
   render() {
+    if (this.props.skateRoutes) {
+      console.log(this.props.skateRoutes);
+    }
+
+    const skateRoutes = Object.values(this.props.skateRoutes);
+
     return (
       <div>
+        <ul>
+          {skateRoutes.map(route => <RouteIndexItem key={route.id} skateRoute={route} />)}
+
+        </ul>
       </div>
     );
   }
 }
- export default RouteIndex;
+
+export default RouteIndex;
