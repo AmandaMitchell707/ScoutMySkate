2996cf3f4f584ed1545d0c9125655fee1d8fc203
Amanda Mitchell <amandamitchell707@gmail.com>
2018-12-21 15:07:50 -0800

    Convert route index item from functional component to class component

--- frontend/components/routes/route_index_item.jsx
+++ frontend/components/routes/route_index_item.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const RouteIndexItem = ({ skateRoute }) => (
-  <li>
-    <Link to={`/routes/view/${skateRoute.id}`}>
-      <div>{skateRoute.name}</div>
-      <div>{skateRoute.distance}</div>
-    </Link>
-  </li>
-);
+class RouteIndexItem extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const skateRoute = this.props.skateRoute;
+
+    return (
+      <li>
+        <Link to={`/routes/view/${skateRoute.id}`}>
+          <div>{skateRoute.name}</div>
+          <div>{skateRoute.distance}</div>
+          <div>{skateRoute.city}</div>
+          <div>{skateRoute.createdAt}</div>
+        </Link>
+      </li>
+    )
+  }
+}
 
 export default RouteIndexItem;
