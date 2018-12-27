3f1267bca3e3927334bca73bc5c1e0efe55decf7
Amanda Mitchell <amandamitchell707@gmail.com>
2018-12-26 18:27:27 -0800

    Add header bar for routes index

--- frontend/components/routes/route_index.jsx
+++ frontend/components/routes/route_index.jsx
@@ -11,10 +11,19 @@ class RouteIndex extends React.Component {
     const skateRoutes = Object.values(this.props.skateRoutes);
 
     return (
-      <div>
+      <div className="middle-container">
+        <div className="my-routes-title-container">
+          <h3 className="my-routes-title"><span>MY ROUTES</span></h3>
+          <div className="create-route">
+            <button className="create-route-button">CREATE A ROUTE</button>
+          </div>
+        </div>
+
+      <section className="results-container">
         <ul>
           {skateRoutes.map(route => <RouteIndexItem key={route.id} skateRoute={route} />)}
         </ul>
+      </section>
       </div>
     );
   }
