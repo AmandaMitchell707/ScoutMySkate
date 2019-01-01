957712e9dfa72e4ad810f4a017ac31984b346147
Amanda Mitchell <amandamitchell707@gmail.com>
2019-01-01 12:28:39 -0800

    Add remove skate route action type

--- frontend/reducers/skate_routes_reducer.js
+++ frontend/reducers/skate_routes_reducer.js
@@ -2,20 +2,25 @@ import merge from 'lodash/merge';
 
 import {
   RECEIVE_SKATE_ROUTE,
-  RECEIVE_SKATE_ROUTES
+  RECEIVE_SKATE_ROUTES,
+  REMOVE_SKATE_ROUTE
 } from '../actions/skate_route_actions';
 
 const skateRoutesReducer = (state = {}, action) => {
   Object.freeze(state);
+  let newState;
 
   switch (action.type) {
     case RECEIVE_SKATE_ROUTE:
-      let newState = merge({}, state);
+      newState = merge({}, state);
       newState[action.skateRoute.id] = action.skateRoute;
       return newState;
-      // return merge({}, state, { [action.skateRoute.id]: action.skateRoute });
     case RECEIVE_SKATE_ROUTES:
       return action.skateRoutes;
+    case REMOVE_SKATE_ROUTE:
+      newState = merge({}, state);
+      delete newState[action.routeId];
+      return newState;
     default:
       return state;
   }
